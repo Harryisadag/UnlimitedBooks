5c3a1f5f2f0be757e21e1f1e940fede5082ab933
Harryisadag <98896307+Harryisadag@users.noreply.github.com>
2025-04-08 16:04:36 -0400

    redid again again

--- gradlew
+++ gradlew
@@ -1,3 +1,4 @@
-#!/bin/sh
-DIR=$(cd "$(dirname "$0")" && pwd)
-exec "$DIR/gradle/wrapper/gradle-wrapper.jar" "$@"
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+java -Xmx1G -Xms128M -jar "$DIR/gradle/wrapper/gradle-wrapper.jar" "$@"
---
+++ src/main/java/com/harryisadag/unlimitedbooks/UnlimitedBooks.java
@@ -0,0 +1,10 @@
+package com.harryisadag.unlimitedbooks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UnlimitedBooks implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("UnlimitedBooks mod initialized!");
+    }
+}
